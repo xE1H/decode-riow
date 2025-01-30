71e49697e0c88fbd57f9687cb616fd273a49fa21
nojus <me@nojus.dev>
2025-01-30 22:50:16 +0200

    cleanup teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VLRTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VLRTeleOp.java
@@ -1,28 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.outoftheboxrobotics.photoncore.Photon;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.teamcode.auto.pedroPathing.localization.GoBildaPinpointDriver;
-import org.firstinspires.ftc.teamcode.auto.pedroPathing.localization.localizers.PinpointLocalizer;
 import org.firstinspires.ftc.teamcode.controls.PrimaryDriverTeleOpControls;
 import org.firstinspires.ftc.teamcode.controls.SecondaryDriverTeleOpControls;
 import org.firstinspires.ftc.teamcode.helpers.opmode.VLRLinearOpMode;
 import org.firstinspires.ftc.teamcode.helpers.subsystems.VLRSubsystem;
 import org.firstinspires.ftc.teamcode.helpers.utils.GlobalConfig;
 import org.firstinspires.ftc.teamcode.subsystems.arm.ArmState;
-import org.firstinspires.ftc.teamcode.subsystems.arm.commands.ResetSlide;
 import org.firstinspires.ftc.teamcode.subsystems.arm.rotator.ArmRotatorSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.arm.slide.ArmSlideSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.chassis.Chassis;
 import org.firstinspires.ftc.teamcode.subsystems.claw.ClawSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.hang.HangSubsystem;
-import org.firstinspires.ftc.teamcode.subsystems.vision.Vision;
 
 
 /**
@@ -66,12 +58,6 @@ public class VLRTeleOp extends VLRLinearOpMode {
         while (opModeIsActive()) {
             primaryDriver.update();
             secondaryDriver.update();
-
-            if (GlobalConfig.DEBUG_MODE) {
-                telemetry.addData("current state", ArmState.get());
-//                telemetry.addData("Slide pos", ass.getPosition());
-            }
-            telemetry.update();
         }
     }
 }
