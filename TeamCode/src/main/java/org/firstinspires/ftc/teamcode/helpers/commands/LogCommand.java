49dde68d127f70639743b127750e8b794c17e6a1
nojus <me@nojus.dev>
2025-04-21 17:13:21 +0300

    add lambda support for LogCommand

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/commands/LogCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/commands/LogCommand.java
@@ -2,28 +2,35 @@ package org.firstinspires.ftc.teamcode.helpers.commands;
 
 import org.firstinspires.ftc.teamcode.helpers.subsystems.VLRSubsystem;
 
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LogCommand extends InstantCommand {
     private final Logger logger;
     private final Level level;
-    private final String message;
+    private final Supplier<String> message;
 
     public LogCommand(Class<? extends VLRSubsystem<?>> subsystem, Level level, String message) {
         this.logger = VLRSubsystem.getLogger(subsystem);
         this.level = level;
-        this.message = message;
+        this.message = () -> message;
     }
 
     public LogCommand(String loggerName, Level level, String message) {
         this.logger = Logger.getLogger(loggerName);
+        this.level = level;
+        this.message = () -> message;
+    }
+
+    public LogCommand(Class<? extends VLRSubsystem<?>> subsystem, Level level, Supplier<String> message) {
+        this.logger = VLRSubsystem.getLogger(subsystem);
         this.level = level;
         this.message = message;
     }
 
     @Override
     public void run() {
-        logger.log(level, message);
+        logger.log(level, message.get());
     }
 }
